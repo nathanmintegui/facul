1fed2ee0101cf11fc954c99342c3344e3d71287e
nathanmintegui <nathanberger.rs@hotmail.com>
2024-09-28 01:24:40 -0300

    feat: add comments

--- main.c
+++ main.c
@@ -2,15 +2,16 @@
 #include <stdio.h>
 
 const float valorIngresso = 30.50;
-const int idadeMinimaMeiaEntrada = 10;
-const int idadeMaximaMeiaEntrada = 65;
+const int IDADE_MINIMA_MEIA_ENTRADA = 10;
+const int IDADE_MAXIMA_MEIA_ENTRADA = 65;
 
 char arr[4][20] = {"É assim que acaba", "Deadpool e Wolwerine",
                    "Divertidamente 2", "Armadilha"};
 
+/* Calcula o valor do ingresso levando em conta a meia-entrada */
 float calcularValorIngresso(int idade) {
   bool isMeiaEntrada =
-      idade < idadeMinimaMeiaEntrada || idade > idadeMaximaMeiaEntrada;
+      idade < IDADE_MINIMA_MEIA_ENTRADA || idade > IDADE_MAXIMA_MEIA_ENTRADA;
 
   if (isMeiaEntrada) {
     return valorIngresso / 2.0;
@@ -19,6 +20,7 @@ float calcularValorIngresso(int idade) {
   return valorIngresso;
 }
 
+/* Exibe os filmes disponíveis do cartaz */
 void renderizarFilmesEmCartaz() {
   printf("Opções de Filmes Disponíveis\n");
 
@@ -29,43 +31,50 @@ void renderizarFilmesEmCartaz() {
   printf("\n");
 }
 
-char *getFilmeEscolhido(int opcao) { return arr[opcao - 1]; }
+/* Retorna o nome do filme baseado na opção */
+char *getFilmeEscolhido(int opcao) {
+  return arr[opcao - 1];
+}
 
 int main(void) {
-  int opcao;
-  int quantidade;
-  int idade;
-  float valorTotal;
+  /* Inicializa as variáveis */
+  int opcao,quantidade, idade;
+  float valorTotal = 0.0f;
 
   renderizarFilmesEmCartaz();
 
   printf("Escolha uma das opções acima: ");
   scanf("%d", &opcao);
 
+  /* Verifica se a opção informada é valida */
   if (opcao < 1 || opcao > 4) {
-    printf("Opção inválida");
-    return 0;
+    printf("Opção inválida!\n");
+    return 1;
   }
 
   printf("Quantos ingressos: ");
   scanf("%d", &quantidade);
 
+  /* Seta quantidade como 1 caso o número fornecido seja menor que 1 */
   if (quantidade < 1) {
     quantidade = 1;
   }
 
+  /* Soma o valor de cada ingresso de acordo com a quantidade*/
   for (int idx = 1; idx <= quantidade; idx++) {
     printf("Ingresso %d -> quantos anos? ", idx);
     scanf("%d", &idade);
 
+  /* Verifica se a idade informada é valida */
     if (idade < 1 || idade > 140) {
-      printf("Idade inválida");
-      return 0;
+      printf("Idade inválida!\n");
+      return 1;
     }
 
     valorTotal += calcularValorIngresso(idade);
   }
 
+  /* Exibe o resultado final */
   char *filmeEscolhido = getFilmeEscolhido(opcao);
 
   printf("Você escolheu o filme:\n%s, \no valor total é de R$%.2f\n",
